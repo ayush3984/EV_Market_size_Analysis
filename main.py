f3a1bceaf5cfc8461cef45868053429394eedb0f
ayush3984 <ayushmaurya3984@gmail.com>
2025-12-10 13:43:42 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-sns.set_style("whitegrid")
-
-df = pd.read_csv("Electric_Vehicle_Population_Data.csv")
-df.head()
